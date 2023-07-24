282759b41d4afcbb35b9a06e138a65fdba93d40a
tang <1942183657@qq.com>
2023-07-24 15:01:40 +0800

    BiTree：析构函数，递归后序遍历。 NOTE: 回调函数是C中的技术，C++不是特别支持。类普通成员函数不能直接作为回调函数，而类静态成员函数又不能直接访问类的非静态成员（可把该类的对象作为该函数参数）。

--- BinaryTree/BiTree.h
+++ BinaryTree/BiTree.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 
+
 template<class T> class BiNode;
 template<class T>
 class BiNode {
@@ -33,46 +34,63 @@ public:
 		root(nullptr), count(0)
 	{}
 
+	void traversePostOrder_for_Delete(BiNode<T>* parent);
+
+	/*
+	 * 只能用后序遍历去释放树中的结点，不同于只遍历不释放，释放之后结点的指针域也访问不了。
+	*/
 	~BiTree()
 	{
-	
+		traversePostOrder_for_Delete(root);
 	}
 
 	const int& getCount() {
 		return count;
 	}
 
+	/*
+	* 用顺序存储二叉树初始化一个二叉链表存储二叉树
+	*/
 	void createBiTree_from_SqBiTree(SqBiTree<T>& sqBiTree, int i, BiNode<T>*& parent);//定义两个特例化
-
 	void createBiTree_from_SqBiTree(SqBiTree<T>& sqBiTree);//为了封装上面的同名函数，因为不能把成员变量root作为成员函数参数默认值
 
-	//todo:层序遍历，图形化
-	void traversePreOrder(BiNode<T>* parent) {
-		if (parent) {
-			std::cout << parent->data << std::endl;
-			traversePreOrder(parent->lchild);
-			traversePreOrder(parent->rchild);
-		}
-	}
-
+	/*
+	* 以先序序列的顺序把元素输出
+	*/
+	void traversePreOrder(BiNode<T>* parent);
 	void traversePreOrder() {
+		std::cout << "先序遍历序列：";
 		traversePreOrder(root);
+		std::cout << std::endl;
 	}
 
-	void printAlltoVecPreOrder(BiNode<T>* parent, std::vector<T>& vec) {
-		if (parent) {
-			vec.push_back(parent->data);
-			printAlltoVecPreOrder(parent->lchild, vec);
-			printAlltoVecPreOrder(parent->rchild, vec);
-		}
-	}
-
+	/*
+	* 以先序序列的顺序把元素放到vector中
+	*/
+	void printAlltoVecPreOrder(BiNode<T>* parent, std::vector<T>& vec);
 	void printAlltoVecPreOrder(std::vector<T>& vec) {
 		printAlltoVecPreOrder(root, vec);
 	}
+
+	/*
+	* 以后序序列的顺序把元素输出
+	*/
+	void traversePostOrder(BiNode<T>* parent);
+	void traversePostOrder() {
+		std::cout << "后序遍历序列：";
+		traversePostOrder(root);
+		std::cout << std::endl;
+	}
+
+	//todo:层序遍历，图形化输出
+
+	//todo:非递归后序遍历，和相关算法（祖先结点到孩子结点的路径，树高）
 };
 
-//类模板的成员函数的【全-部参数】特例化1
+
+
+
+/*类模板的成员函数的【全 - 部参数】特例化1 */
 template<>
 inline void BiTree<int>::createBiTree_from_SqBiTree(SqBiTree<int>& sqBiTree, int i, BiNode<int>*& parent) {//note:不能把成员变量root作为成员函数参数默认值
 	if (sqBiTree.dataArr[i] != -1 && i <= sqBiTree.count)
@@ -88,7 +106,7 @@ inline void BiTree<int>::createBiTree_from_SqBiTree(SqBiTree<int>& sqBiTree) {//
 	count = sqBiTree.count;
 }
 
-//类模板的成员函数的【全-部参数】特例化2
+/*类模板的成员函数的【全 - 部参数】特例化2*/
 template<>
 inline void BiTree<char>::createBiTree_from_SqBiTree(SqBiTree<char>& sqBiTree, int i, BiNode<char>*& parent) {
 	if (sqBiTree.dataArr[i] != '*' && i <= count)
@@ -101,3 +119,52 @@ inline void BiTree<char>::createBiTree_from_SqBiTree(SqBiTree<char>& sqBiTree) {
 	createBiTree_from_SqBiTree(sqBiTree, 1, root);
 	count = sqBiTree.count;
 }
+/*
+* 以先序序列的顺序把元素输出
+*/
+template<class T>
+inline void BiTree<T>::traversePreOrder(BiNode<T>* parent) {
+	if (parent) {
+		std::cout << parent->data;
+		traversePreOrder(parent->lchild);
+		traversePreOrder(parent->rchild);
+	}
+}
+
+/*
+* 用后序析构
+*/
+template<class T>
+inline void BiTree<T>::traversePostOrder_for_Delete(BiNode<T>* parent) {
+	if (parent) {
+		traversePostOrder_for_Delete(parent->lchild);
+		traversePostOrder_for_Delete(parent->rchild);
+		delete parent;
+	}
+}
+
+/*
+* 以前序序列的顺序把元素放到vector中
+*/
+template<class T>
+inline void BiTree<T>::printAlltoVecPreOrder(BiNode<T>* parent, std::vector<T>& vec) {
+	if (parent) {
+		vec.push_back(parent->data);
+		printAlltoVecPreOrder(parent->lchild, vec);
+		printAlltoVecPreOrder(parent->rchild, vec);
+	}
+}
+
+/*
+* 以后序序列的顺序把元素输出
+*/
+template<class T>
+inline void BiTree<T>::traversePostOrder(BiNode<T>* parent) {
+	if (parent) {
+		traversePostOrder(parent->lchild);
+		traversePostOrder(parent->rchild);
+		std::cout << parent->data;
+	}
+}
+
+
--- Test/BiTree/testBiTree.cpp
+++ Test/BiTree/testBiTree.cpp
@@ -3,16 +3,24 @@
 using namespace Catch::Matchers;
 
 TEST_CASE("testing for basic operations of BiTree", "") {
-	SqBiTree<int> biTree2(4, 3, { 1,2,3 });
-	
-	BiTree<int> biTree;
-	biTree.createBiTree_from_SqBiTree(biTree2);
+	SECTION("part 1") {
+		SqBiTree<int> sqBiTree(4, 3, { 1,2,3 });
+		BiTree<int> biTree;
+		biTree.createBiTree_from_SqBiTree(sqBiTree);
 
-	//biTree.traversePreOrder();
-	std::vector<int> biTreeVec;
-	biTree.printAlltoVecPreOrder(biTreeVec);
-	std::vector<int> targetVec;
-	targetVec.assign({ 1,2,3 });
-	CHECK_THAT(biTreeVec, Equals(targetVec));
-	CHECK(biTree.getCount() == 3);
+		//biTree.traversePreOrder();
+		std::vector<int> biTreeVec;
+		biTree.printAlltoVecPreOrder(biTreeVec);
+		std::vector<int> targetVec;
+		targetVec.assign({ 1,2,3 });
+		CHECK_THAT(biTreeVec, Equals(targetVec));
+		CHECK(biTree.getCount() == 3);
+	}
+
+	//SECTION("part 2") {
+	//	SqBiTree<int> sqBiTree(8, 7, { 1,-1,3,-1,-1,6,7 });
+	//	BiTree<int> biTree;
+	//	biTree.createBiTree_from_SqBiTree(sqBiTree);
+	//	biTree.traversePostOrder();
+	//}
 }
